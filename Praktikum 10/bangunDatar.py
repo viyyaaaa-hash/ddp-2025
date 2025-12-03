6f44890ea09e4d1d512149f9a1aaeccb94d3dcfb
viyyaaaa hash <viyyaaaa@gmail.com>
2025-12-03 11:45:21 +0700

    upload file

---
+++ Praktikum 10/bangunDatar.py
@@ -0,0 +1,16 @@
+import math
+
+def JajarGenjang(alas, tinggi):
+    return alas * tinggi 
+
+def persegi(sisi):
+    return sisi * sisi
+
+def persegi_panjang(panjang, lebar):
+    return panjang * lebar
+
+def segitiga(alas, tinggi):
+    return alas * tinggi / 2
+
+def lingkaran(jari_jari):
+    return math.pi * jari_jari**2
